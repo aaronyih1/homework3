281116a90c94aee8a34a51881d68bc9692f1f476
Aaron Yih <aaron.yih@ucla.edu>
2015-04-27 18:50:15 -0700

    first draft

--- 3/guitar.cpp
+++ 3/guitar.cpp
@@ -65,6 +65,70 @@ int emptyBeats(string tuneInput)
     }
     return(0);
 }
+int tuneEndsEarly(string tuneInput)
+{
+    int i = tuneInput.length()-1;
+    cerr << i << "  ";
+    int counter = 0;
+    int counter2 = 0;
+    do
+    {
+        counter++;
+        i--;
+    }
+    while(tuneInput[i]=='/');
+    cerr << counter << " ";
+    if(counter != (tuneInput[i-1]-48))
+    {
+        for(int j = 0; j<tuneInput.length();j++)
+        {
+            if(tuneInput[j] == '/')
+            {
+                counter2++;
+            }
+        }
+    }
+    return(counter2+1);
+}
+string actualTranslation(string inputTune)
+{
+    string output;
+    int slashCounter=0;
+    for(int i = 0; i<inputTune.length(); i++)
+    {
+        if(((isalpha(inputTune[i])&&(islower(inputTune[i]))))||(isalpha(inputTune[i])&&(isupper(inputTune[i]))))
+           {
+               output += tolower(inputTune[i]);
+               slashCounter = 0;
+           }
+        else if(isdigit(inputTune[i]))
+        {
+            for(int j = 0; j<(inputTune[i]-48); j++)
+            {
+                output +=toupper(inputTune[i+1]);
+                cerr << j << endl;
+            }
+            i+=(inputTune[i]-47);
+            slashCounter = 0;
+        }
+        else if(i==0)
+        {
+            output+= 'x';
+            slashCounter++;
+        }
+        else if(inputTune[i]=='/')
+        {
+            slashCounter++;
+            if(slashCounter>1)
+            {
+                output += 'x';
+            }
+            
+        }
+        cerr << i << endl;
+    }
+    return(output);
+}
 /*int translateTune(string tune, string& instructions, int& badBeat){
     if(isTuneWellFormed(tune))
     {
@@ -80,10 +144,15 @@ int emptyBeats(string tuneInput)
         badBeat = findLowest(tune);
         return(2);
     }
- else if(isTuneWellFormed(tune)&& (emptyBeats(tune)>0))
+    else if(isTuneWellFormed(tune)&& (emptyBeats(tune)>0))
     {
         badBeat = emptyBeats(tune);
         return(3);
+    }
+    else if (isTuneWellFormed(tune) && (tuneEndsEarly(tune)>1))
+    {
+        badBeat = tuneEndsEarly(tune);
+        return(4);
     }
 } */
 
@@ -91,8 +160,8 @@ int main() {
     //bool test;
     //test = isTuneWellFormed(" ");
     //cerr << test;
-    int temp;
-    temp = emptyBeats("r//y/3g///o/");
+    string temp;
+    temp = actualTranslation("r//y/3g///o/");
     cerr << temp;
     
 }
